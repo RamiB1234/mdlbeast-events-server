985da70a8867932ec6b1aef9849c3b109a3bf2a3
Rami Bokhari <rbisthebest@hotmail.com>
2024-05-12 19:52:52 +0300

    Remove template files

---
+++ Controllers/EventController.cs
@@ -0,0 +1,24 @@
+﻿using mdlbeast_events_server.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mdlbeast_events_server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : ControllerBase
+    {
+        [HttpGet(Name = "GetEvents")]
+        public List<Event> Get()
+        {
+            return new List<Event> {
+                new Event
+                {
+                    Id=1,
+                    Description= "aaa",
+                    Name= "mdlbeast",
+                    Date="11/11/1111"
+                }
+            };
+        }
+    }
+}
---
+++ Models/Entities/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace mdlbeast_events_server.Models.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Date { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+    }
+}
